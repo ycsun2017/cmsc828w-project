58d59fb588c259e0a67050b9ff42ea0c220d94b6
Jingxi Chen <42823098+codingrex@users.noreply.github.com>
2020-11-11 14:39:21 -0500

    update plot
    add size for all parts (x,y,label)

--- plot.py
+++ plot.py
@@ -53,37 +53,63 @@ def smooth(scalars, weight):  # Weight between 0 and 1
 if __name__ == "__main__":
     n = 10
     s = 2000
-    runs = 2
+    runs = 10
     xs = list(range(s))
-    # for tau in [0.5]:
-        # for every in [25,50]:
-        #     res = read_rewards_multi("results/CartPole-v0_vpg_s{}_n{}_every{}_goal0.5_c0.5_tau{}".format(s,n,every,tau), s, n, runs)
-        #     plt.plot(xs, smooth(res, 0.99), label="every"+str(every))
-        # for every in [25,75]:
-        #     res = read_rewards_multi("results/Swimmer_vpg_s{}_n{}_every{}_size32_c0.5_tau{}".format(s,n,every,tau), s, n, runs)
-        #     plt.plot(xs, smooth(res, 0.9), label="every"+str(every))
 
-    # for tau in [0.5, 0.8]:
+    SMALL_SIZE = 20
+    MEDIUM_SIZE = 25
+    BIGGER_SIZE = 25
+
+    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=MEDIUM_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+# for no_meta
+    # for tau in [0.8]:
     #     for every in [50]:
-    #         res = read_rewards("results/Swimmer_vpg_s{}_n{}_every{}_goal0.5_c0.5_tau{}.txt".format(s,n,every,tau), s, n)
+    #         res = read_rewards_multi("results/n50/Lunar_vpg_s{}_n{}_every{}_size32_c0.5_tau{}".format(s,n,every,tau), s, n, runs)
+    #         plt.plot(xs, smooth(res, 0.99), label="meta")
+    #
+    #
+    # for tau in [0.8]:
+    #     for every in [50]:
+    #         res = read_rewards_multi("results/Lunar_vpg_s{}_n{}_every{}_size32_c0.5_tau{}_nometa".format(s,n,every,tau), s, n, runs)
+    #         plt.plot(xs, smooth(res, 0.99), label="no_meta")
+
+#meta
+    for tau in [0.8]:
+        for every in [50]:
+            res = read_rewards_multi("results/n50/Lunar_vpg_s{}_n{}_every{}_size32_c0.5_tau{}".format(s,n,every,tau), s, n, runs)
+            plt.plot(xs, smooth(res, 0.99), label="every" + str(every))
+
+
+
+
+    # res = read_rewards_multi("results/Lunar_vpg_s{}_n{}_c0.5_maml".format(s,n), s, n, runs)
+    # plt.plot(xs, smooth(res, 0.99), label="every"+str(50))
+
+
+    #     for every in [10,75]:
+    #         res = read_rewards_multi("results/CartPole-v0_vpg_s{}_n{}_every{}_size32_c0.5_tau{}".format(s,n,every,tau), s, n, runs)
+    #         plt.plot(xs, smooth(res, 0.99), label="every"+str(every))
+
+    # for tau in [0.8]:
+    #     for every in [50]:
+    #         res = read_rewards("results/n50/Lunar_vpg_s{}_n{}_every{}_size32_c0.5_tau{}.txt".format(s,n,every,tau), s, n)
     #         plt.plot(xs, smooth(res, 0.99), label="tau"+str(tau))
 
-    
-    # for tau in [0.8]:
-    #     for every in [25,75]:
-    #         res = read_rewards("results/Swimmer_vpg_s{}_n{}_every{}_size32_c0.5_tau{}.txt".format(s,n,every,tau), s, n)
-    #         plt.plot(xs, smooth(res, 0.99), label="every"+str(every))
-    res = read_rewards("results_peihong/maml_CartPole-v0_vpg_s2000_n10_every75_size32.txt", s, n)
-    plt.plot(xs, smooth(res, 0.99), label="nometa")
+##
 
 
-    # res = read_rewards("results_peihong/CartPole-v0_vpg_s2000_n10_c0.5.txt", s, n)
-    # plt.plot(xs, smooth(res, 0.99))
 
-##    
     plt.legend()
-    plt.xlabel("samples (envs)")
-    plt.ylabel("reward")
+    plt.xlabel("Tasks (environments)")
+    plt.ylabel("Mean reward")
     plt.show()
 #    plt.savefig("plots/hybird_vpg_cart.png", format="png")
     
